a8fb0bd82a425ba3c84ab7c094cf771a486b1a84
nicolas.souza <nicolassouza144@gmail.com>
2020-09-15 21:43:05 -0300

    add role attribute

--- src/main/java/com/spring/course/domain/User.java
+++ src/main/java/com/spring/course/domain/User.java
@@ -3,6 +3,8 @@ package com.spring.course.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.spring.course.domain.enums.Role;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,7 @@ public class User {
 	private String name;
 	private String email;
 	private String password;
+	private Role role;
 	private List<Request> requests = new ArrayList<>();
 	private List<RequestStage> stages = new ArrayList<>();
 }
