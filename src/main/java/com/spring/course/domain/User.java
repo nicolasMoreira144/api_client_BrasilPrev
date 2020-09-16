43744611a37aada7f1d9908a0b6763f86eaa1151
nicolas.souza <nicolassouza144@gmail.com>
2020-09-16 10:27:07 -0300

    Map entities

--- src/main/java/com/spring/course/domain/User.java
+++ src/main/java/com/spring/course/domain/User.java
@@ -3,6 +3,15 @@ package com.spring.course.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import com.spring.course.domain.enums.Role;
 
 import lombok.AllArgsConstructor;
@@ -13,13 +22,31 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity(name = "user")
 public class User {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String name;
+	
+	@Column(length = 75, nullable = false, unique = true)
 	private String email;
+	
+	@Column(length = 100, nullable = false)
 	private String password;
+	
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
+	
+	// um usuario pode ter muitos pedidos
+	@OneToMany(mappedBy = "user") // ver como esta refenciado o atributo user(this.name in attribute) na classe Request
 	private List<Request> requests = new ArrayList<>();
+	
+	// um usuario pode ter muitos estagio pedido
+	@OneToMany(mappedBy = "user")
 	private List<RequestStage> stages = new ArrayList<>();
 }
