a9e3dc17728342a312f180b808db37011df1ffd7
nicolas.souza <nicolassouza144@gmail.com>
2020-09-17 19:47:25 -0300

    create request repository

---
+++ src/main/java/com/spring/course/repository/RequestRepository.java
@@ -0,0 +1,20 @@
+package com.spring.course.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.spring.course.domain.Request;
+import com.spring.course.domain.enums.RequestState;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long> {
+	
+	public List<Request> findAllByOwnerId(Long id);
+	
+	@Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+	public Request updateStatus(Long id, RequestState state);
+
+}
--- src/main/java/com/spring/course/repository/UserRepository.java
+++ src/main/java/com/spring/course/repository/UserRepository.java
@@ -1,3 +1,4 @@
+package com.spring.course.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
