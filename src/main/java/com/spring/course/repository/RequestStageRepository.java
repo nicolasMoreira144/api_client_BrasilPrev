c958ee85287a51a377e343d5fa6ef4ca8679d880
nicolas.souza <nicolassouza144@gmail.com>
2020-09-17 19:47:55 -0300

    create request stage repository

---
+++ src/main/java/com/spring/course/repository/RequestStageRepository.java
@@ -0,0 +1,14 @@
+package com.spring.course.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.course.domain.RequestStage;
+
+@Repository
+public interface RequestStageRepository extends JpaRepository<RequestStage, Long> {
+	
+	public List<RequestStage> findAllByRequestId(Long id);
+}
