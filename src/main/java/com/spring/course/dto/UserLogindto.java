1442deb01bb2b59f8e0f961ce2ef261c00a92a2f
nicolas.souza <nicolassouza144@gmail.com>
2020-10-07 08:24:48 -0300

    Add attribute validation

--- src/main/java/com/spring/course/dto/UserLogindto.java
+++ src/main/java/com/spring/course/dto/UserLogindto.java
@@ -1,5 +1,8 @@
 package com.spring.course.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +12,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter @Setter
 public class UserLogindto {
+	
+	@Email(message = "Invalid email address")
 	private String email;
+	
+	@NotBlank(message = "Password required")
 	private String password;
 
 }
