a2cce949f7d3277d531d828dabc552fc311d79e2
nicolas.souza <nicolassouza144@gmail.com>
2020-10-15 15:19:19 -0300

    setting environments

---
+++ src/main/java/com/spring/course/profiles/DBConfiguration.java
@@ -0,0 +1,26 @@
+package com.spring.course.profiles;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring-datasource")
+public class DBConfiguration {
+
+
+	@Profile("dev")
+	@Bean
+	public String devDatabaseConnection() {
+		System.out.println("test conection database dev ---------------->>>>>>>>>>>>>>>");
+		return "";
+	}
+
+	@Profile("prod")
+	@Bean
+	public String prodDatabaseConnection() {
+		System.out.println("test conection database production ---------------->>>>>>>>>>>>>>>");
+		return "";
+	}
+}
