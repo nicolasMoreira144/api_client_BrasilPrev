c00898d99dfda8b9be2d8002f124861ad9234b8e
nicolas.souza <nicolassouza144@gmail.com>
2020-10-22 12:31:58 -0300

    Protect api resource

--- src/main/java/com/spring/course/security/SecurityConfig.java
+++ src/main/java/com/spring/course/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.spring.course.service.UserService;
 
@@ -35,6 +36,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests()
+			.anyRequest().authenticated();
+		
+		http.addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+	
 	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
