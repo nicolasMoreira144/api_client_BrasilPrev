5f56386323f07f2c3113794427f21ba24e07d83b
nicolas.souza <nicolassouza144@gmail.com>
2020-10-23 07:56:57 -0300

    Enable global method security

--- src/main/java/com/spring/course/resource/UserResource.java
+++ src/main/java/com/spring/course/resource/UserResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -51,6 +52,7 @@ public class UserResource {
 	@Autowired
 	private JwtManager jwtManager; 
 	
+	@Secured({"ROLE_ADMINISTRATOR"})
 	@PostMapping
 	public ResponseEntity<User> save(@RequestBody @Valid UserSavedto userdto) {
 		User userToSave = userdto.transformToUser();
@@ -111,7 +113,8 @@ public class UserResource {
 		PageModel<Request> pm = requestService.listAllByOwnerIdOnLazyModel(ownerId, pr);
 		return ResponseEntity.ok(pm);
 	}
-
+	
+	@Secured({"ROLE_ADMINISTRATOR"})
 	@PatchMapping("/role/{id}")
 	public ResponseEntity<?> updateRole(@PathVariable(name = "id") Long id, @RequestBody @Valid UserUpdateRoledto userdto){
 		User user = new User();
